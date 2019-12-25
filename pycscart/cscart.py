98225d21b30a96dd4c4d20f9b8b0e246528ff4df
Richard Ma <richard.ma.19850509@gmail.com>
2019-12-25 10:19:32 +0800

    v0.1

--- pycscart/cscart.py
+++ pycscart/cscart.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import json
 
 
 class APIException(Exception):
@@ -21,7 +22,9 @@ class Cscart:
         self.authorization = "Basic " + base64.b64encode(
             f"{email}:{api_key}".encode("utf-8")).decode("utf-8")
 
-        self.headers = {"authorization": self.authorization}
+        self.headers = {
+            "authorization": self.authorization,
+        }
 
     def _api_prefix(self) -> str:
         if self.version == 1:
@@ -34,8 +37,7 @@ class Cscart:
     def _url(self, api: str) -> str:
         return self.url_base + api
 
-    def _check_response(self, req: requests.Response,
-                        success_status_code: int,
+    def _check_response(self, req: requests.Response, success_status_code: int,
                         error_message: str) -> requests.Response:
         if req.status_code == success_status_code:
             return req
@@ -45,22 +47,26 @@ class Cscart:
     def _get(self, api: str) -> requests.Response:
         url = self._url(api)
         req = requests.get(url, headers=self.headers)
-        return self._check_response(req, 200, "GET "+url)
+        return self._check_response(req, 200, "GET " + url)
 
     def _delete(self, api: str) -> requests.Response:
         url = self._url(api)
         req = requests.delete(url, headers=self.headers)
-        return self._check_response(req, 204, "DELETE "+url)
+        return self._check_response(req, 204, "DELETE " + url)
 
     def _post(self, api: str, data: dict) -> requests.Response:
         url = self._url(api)
-        req = requests.post(url, data=data, headers=self.headers)
-        return self._check_response(req, 201, "CREATE "+url)
+        self.headers.update({'Content-Type': 'application/json'})
+        req = requests.post(url, json=data, headers=self.headers)
+        del self.headers['Content-Type']
+        return self._check_response(req, 201, "CREATE " + url)
 
     def _put(self, api: str, data: dict) -> requests.Response:
         url = self._url(api)
-        req = requests.put(url, data=data, headers=self.headers)
-        return self._check_response(req, 200, "UPDATE "+url)
+        self.headers.update({'Content-Type': 'application/json'})
+        req = requests.put(url, json=data, headers=self.headers)
+        del self.headers['Content-Type']
+        return self._check_response(req, 200, "UPDATE " + url)
 
     def get_orders(self) -> dict:
         api = "/orders"
@@ -74,8 +80,31 @@ class Cscart:
         api = "/orders/" + order_id
         self._delete(api)
 
+    def create_order(self, company_id: str, data: dict) -> dict:
+        api = "/stores/%s/orders" % (company_id)
+        return self._post(api, data).json()
+
+    def update_order(self, order_id: str, data: dict) -> dict:
+        api = "/orders/" + order_id
+        return self._put(api, data).json()
 
 if __name__ == "__main__":
     c = Cscart(domain="http://clothingfan.com",
                email="mywebadmin@admin.com",
                api_key="v21AY99014bJ76133UZhgCl46AB66G07")
+    #print(c.get_order('1210818'))
+    #data = {
+        #"user_id": "6993",
+        #"shipping_id": "4",
+        #"payment_id": "21",
+        #"products": {
+            #"205693": {
+                #"amount": "1"
+            #}
+        #}
+    #}
+    #print(c.create_order("1", data))
+    data = {
+            "status": "O"
+    }
+    print(c.update_order('1210826', data))
